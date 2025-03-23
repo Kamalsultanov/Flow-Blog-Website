1878bc6592fe095160459002a66c47416ca31414
Expep <89018966+Expep@users.noreply.github.com>
2025-03-23 23:11:38 +0400

    Fix

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <section className="w-full h-full flex items-center justify-center ">
+    <section className="w-full h-screen flex items-center justify-center ">
       <div
         className="w-32 h-32 border-4 border-dashed rounded-full animate-spin border-aqua  "
         bis_skin_checked="1"
